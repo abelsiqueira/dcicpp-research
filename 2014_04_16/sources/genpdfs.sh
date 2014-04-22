a89316c56d3572e376f3396a54d471a587154b7e
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-04-22 16:28:11 -0300

    Update report of 2014-04-16 to include the `Algencan` comparison

--- 2014_04_16/sources/genpdfs.sh
+++ 2014_04_16/sources/genpdfs.sh
@@ -19,35 +19,43 @@ EOF
 pdflatex -interaction batchmode -output-directory /tmp/ empty.tex
 
 dates="2014.04.16"
+
+suffix=(xf ov)
+options=('' '--compare optimalvalues --infeasibility-tolerance 1e-4')
+
 for date in $dates
 do
   dir=compare.$date
+
   mkdir -p /tmp/$dir
 
-  name=all
-  outname=$(echo $name.$date | tr . _).pdf
-
-  perprof @perprof.args $dir/cute* -o /tmp/$dir/$name $* \
-    --success "Optimal,Converged" --mintime 0.005
-  if [ -f /tmp/$dir/$name.pdf ]; then
-    cp -f /tmp/$dir/$name.pdf ../plots/$outname
-  else
-    echo "Copying empty.pdf => $outname"
-    cp -f /tmp/empty.pdf ../plots/$outname
-  fi
-  for list in $(ls $dir/lists/*.list)
+  for n in 0 1
   do
-    name=${list/$dir\/lists\//}
-    name=${name/.list/}
+    cmd="perprof @perprof.args $dir/cute* --success Optimal,Converged \
+      --mintime 0.005 --maxtime 900 ${options[$n]}"
+    name=all_${suffix[$n]}
     outname=$(echo $name.$date | tr . _).pdf
 
-    perprof @perprof.args $dir/cute* -o /tmp/$dir/$name $* \
-      --success "Optimal,Converged" --subset $list --mintime 0.005
+    $cmd -o /tmp/$dir/$name $*
     if [ -f /tmp/$dir/$name.pdf ]; then
       cp -f /tmp/$dir/$name.pdf ../plots/$outname
     else
       echo "Copying empty.pdf => $outname"
       cp -f /tmp/empty.pdf ../plots/$outname
     fi
+    for list in $(ls $dir/lists/*.list)
+    do
+      name=${list/$dir\/lists\//}
+      name=${name/.list/}_${suffix[$n]}
+      outname=$(echo $name.$date | tr . _).pdf
+
+      $cmd -o /tmp/$dir/$name $* --subset $list
+      if [ -f /tmp/$dir/$name.pdf ]; then
+        cp -f /tmp/$dir/$name.pdf ../plots/$outname
+      else
+        echo "Copying empty.pdf => $outname"
+        cp -f /tmp/empty.pdf ../plots/$outname
+      fi
+    done
   done
 done
