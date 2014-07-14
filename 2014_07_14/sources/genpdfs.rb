3f897c4806a9d1df70a4ace9f1d98d4a4a1c3df0
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-07-14 11:03:39 -0300

    Add incomplete tests from 2014-07-14

---
+++ 2014_07_14/sources/genpdfs.rb
@@ -0,0 +1,97 @@
+#!/usr/bin/env ruby
+
+def create_samef_list(dir, ftol, f0, files)
+  cmd = "./find-equal-fval.rb #{files} --ftol #{ftol} --f0 #{f0}"
+  samef = sprintf("samef_%1.0e_%1.0e", ftol, f0)
+  sameflist = "#{dir}/lists/#{samef}.list"
+  if File.exist?(sameflist)
+    `mkdir $(dirname /tmp/#{sameflist}`
+    `#{cmd} > /tmp/#{sameflist}`
+    if `diff /tmp/#{sameflist} #{sameflist}`
+      return
+    end
+    `mv -f /tmp/#{sameflist} #{sameflist}`
+  else
+    `#{cmd} > #{sameflist}`
+  end
+  lists = `ls #{dir}/lists/*.list`.split
+  lists.each do |list|
+    listname = list[/#{dir}.lists.(.*).list/,1]
+    unless list.include?("samef")
+      `sif-intersect-lists.sh #{list} #{sameflist} > #{dir}/lists/#{samef}_#{listname}.list`
+    end
+  end
+end
+
+open('perprof.args','w') do |file|
+  file.puts('--tikz')
+  file.puts('--semilog')
+  file.puts('--free-format')
+end
+
+open('empty.tex','w') do |file|
+  file.puts('\documentclass[border={0 0.5cm 0cm 0.5cm}]{standalone}')
+  file.puts('\usepackage[utf8]{inputenc}')
+  file.puts('\usepackage[T1]{fontenc}')
+  file.puts('\begin{document}')
+  file.puts('EMPTY')
+  file.puts('\end{document}')
+end
+
+`pdflatex -interaction batchmode -output-directory /tmp/ empty.tex`
+
+dirs=`find . -name "comp*"`.split.sort
+`mkdir -p ../plots`
+
+#suffixes=['xf', 'ov']
+#options=['', '--compare optimalvalues --infeasibility-tolerance 1e-4']
+suffixes=['xf']
+options=['']
+
+ftols = [5e-3]
+f0s = [1e-5]
+
+dirs.each do |dir|
+  `rm -f #{dir}/lists/samef_*`
+  files = (ARGV.empty? ? `ls #{dir}/cute*`.split : ARGV).join(' ')
+
+  ftols.each do |ftol|
+    f0s.each do |f0|
+      create_samef_list(dir, ftol, f0, files)
+    end
+  end
+
+  date = dir[/\.\/compare.(.*)/,1]
+
+  lists = [''] + `ls #{dir}/lists/*.list`.split
+
+  `mkdir -pv /tmp/#{dir}/`
+
+  suffixes.each_with_index do |suffix,i|
+    cmd = "perprof @perprof.args #{files} --success Converged,convergence" +
+      " --mintime 0.005 --maxtime 900 #{options[i]}"
+
+    lists.each do |list|
+      if list == ''
+        name = 'all'
+        s = ''
+      else
+        name = list[/#{dir}\/lists\/(.*).list/,1]
+        s = "--subset #{list}"
+      end
+      name = name + '_' + suffix + '_' + date
+      name.gsub!('.','_')
+
+      `#{cmd} -f -o /tmp/#{dir}/#{name} #{s}`
+      if File.exist?("/tmp/#{dir}/#{name}.pdf")
+        puts "Creating #{name}"
+        `cp -f /tmp/#{dir}/#{name}.pdf ../plots/#{name}.pdf`
+      else
+        puts "Copying empty.pdf => #{name}"
+        `cp -f /tmp/empty.pdf ../plots/#{name}.pdf`
+      end
+    end
+  end
+end
+
+`./gen.results.rb`
