7b53b052cf673f346dc3428a612991028fcd4c4d
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-08-18 23:17:52 -0300

    Update tests

--- 2014_07_14/sources/gen.lists.sh
+++ 2014_07_14/sources/gen.lists.sh
@@ -4,8 +4,11 @@ slack="false"
 
 for dir in $(ls | grep compare)
 do
-  awk '{if ($3 <= 0.005) print $1}' $dir/cutest.algencan > /tmp/toosmall.list
-  #echo "" > /tmp/toosmall.list
+  if [ -f $dir/cutest.algencan ]; then
+    awk '{if ($3 <= 0.005) print $1}' $dir/cutest.algencan > /tmp/toosmall.list
+  else
+    echo "" > /tmp/toosmall.list
+  fi
   sif-invert-list.sh /tmp/toosmall.list > $dir/lists/set.list
   grep cholok $dir/cutest.dcicpp | awk '{print $1}' > /tmp/fullrank.list
   cat backup.lists/var1e{0,2}*_con1e{0,2}*${slack}.list > /tmp/nolarge.list
@@ -20,5 +23,12 @@ do
   do
     sif-intersect-lists.sh $dir/lists/{$list,fullrank}.list > \
       $dir/lists/fullrank_${list}.list
+  done
+  for list in nofix onlyfix
+  do
+    sif-intersect-lists.sh $dir/lists/{nolarge,$list}.list > \
+      $dir/lists/nolarge_$list.list
+    sif-intersect-lists.sh $dir/lists/{nolarge_$list,fullrank}.list > \
+      $dir/lists/fullrank_nolarge_$list.list
   done
 done
