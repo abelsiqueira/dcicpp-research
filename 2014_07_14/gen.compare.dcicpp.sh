bcb32ec33a51ec1dc390c6212c85b8a6efd1670b
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-08-25 16:09:29 -0300

    Update date of report

--- 2014_07_14/gen.compare.dcicpp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sed 's/_14.pdf/_14_dcicpp.pdf/g' report-2014-07-14.tex > \
-  report-2014-07-14-dcicpp.tex
-sed 's/07_14.pdf/08_21_dcicpp.pdf/g' report-2014-07-14.tex > \
-  report-2014-08-21-dcicpp.tex
